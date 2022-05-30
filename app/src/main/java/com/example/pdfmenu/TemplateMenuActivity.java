a3db9c9c7163d6e925d9608847489ea52b837fe7
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2022-05-30 02:16:21 +0200

    Added cardView

--- app/src/main/java/com/example/pdfmenu/TemplateMenuActivity.java
+++ app/src/main/java/com/example/pdfmenu/TemplateMenuActivity.java
@@ -1,16 +1,24 @@
 package com.example.pdfmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class TemplateMenuActivity extends AppCompatActivity {
 
+    RecyclerView menusList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_template_menu);
+
+        menusList = findViewById(R.id.menu_recycler_view);
+
+
     }
 
     public void sendMessage(View view) {
