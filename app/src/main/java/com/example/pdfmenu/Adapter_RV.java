6b8bf6574645e5a987d2d3b6fda5bd8e7480e4af
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2022-05-30 03:22:26 +0200

    Added first version of menu templates

---
+++ app/src/main/java/com/example/pdfmenu/Adapter_RV.java
@@ -0,0 +1,61 @@
+package com.example.pdfmenu;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter_RV extends RecyclerView.Adapter<Adapter_RV.ViewHolder> {
+
+    List<String> titles;
+    List<Integer> images;
+    Context context;
+    LayoutInflater inflater;
+
+    public Adapter_RV(Context cntx, List<String> titles, List<Integer> images){
+        this.titles = titles;
+        this.images = images;
+        this.inflater = LayoutInflater.from(cntx);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.menu_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(titles.get(position));
+        holder.gridImage.setImageResource(images.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        ImageView gridImage;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.card_name_text_view);
+            gridImage = itemView.findViewById(R.id.card_image);
+
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/pdfmenu/TemplateMenuActivity.java
+++ app/src/main/java/com/example/pdfmenu/TemplateMenuActivity.java
@@ -1,15 +1,22 @@
 package com.example.pdfmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TemplateMenuActivity extends AppCompatActivity {
 
     RecyclerView menusList;
+    List<String> titles;
+    List<Integer> images;
+    Adapter_RV adapter_rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,25 @@ public class TemplateMenuActivity extends AppCompatActivity {
 
         menusList = findViewById(R.id.menu_recycler_view);
 
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
 
+        titles.add("Example 1");
+        titles.add("Simple menu");
+        titles.add("Wood & red stripes");
+        titles.add("Taxi yellow menu");
+
+        images.add(R.drawable.menu_example_v1);
+        images.add(R.drawable.menu_simple);
+        images.add(R.drawable.menu_wood_red);
+        images.add(R.drawable.menu_yellow);
+
+
+        adapter_rv = new Adapter_RV(this, titles, images);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+        menusList.setLayoutManager(gridLayoutManager);
+        menusList.setAdapter(adapter_rv);
     }
 
     public void sendMessage(View view) {
