1b891deacc91975e71b8da3283b661d09000b765
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2022-06-10 23:59:16 +0200

    Started adding tests

---
+++ app/src/androidTest/java/espressoTests/EspressoTest.java
@@ -0,0 +1,22 @@
+package espressoTests;
+
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.example.pdfmenu.ADGC_Activity;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class EspressoTest {
+    @Rule
+    
+}
