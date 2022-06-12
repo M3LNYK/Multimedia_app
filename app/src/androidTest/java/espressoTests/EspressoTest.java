432d5cd70cebb8f19bf1719ec0980d3566c24d72
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2022-06-12 16:45:00 +0200

    Added few more espresso tests

--- app/src/androidTest/java/espressoTests/EspressoTest.java
+++ app/src/androidTest/java/espressoTests/EspressoTest.java
@@ -1,7 +1,10 @@
 package espressoTests;
 
 import static androidx.test.espresso.Espresso.*;
-import static androidx.test.espresso.assertion.PositionAssertions.isBelow;
+import static androidx.test.espresso.assertion.PositionAssertions.*;
+//import static androidx.test.espresso.assertion.PositionAssertions.isCompletelyAbove;
+//import static androidx.test.espresso.assertion.PositionAssertions.isCompletelyBelow;
+//import static androidx.test.espresso.assertion.PositionAssertions.isTopAlignedWith;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 
@@ -25,12 +28,27 @@ public class EspressoTest {
     public ActivityScenarioRule<ADGC_Activity> activityScenarioRule = new ActivityScenarioRule<ADGC_Activity>(ADGC_Activity.class);
 
     @Test
+    public void searchView_isDisplayedOnTop(){
+        onView(withId(R.id.menu_search)).check(isTopAlignedWith(withId(R.layout.activity_adgc)));
+    }
+
+    @Test
+    public void horizontalScrollView_isBelow_Search(){
+        onView(withId(R.id.horizontal_chip)).check(isCompletelyBelow(withId(R.id.menu_search)));
+    }
+
+    @Test
     public void horizontalScrollView_isDisplayed(){
         onView(withId(R.id.horizontal_chip)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void horizontalScrollView_isBelow_Search(){
-        onView(withId(R.id.horizontal_chip)).check(isBelow(withId(R.id.menu_search)));
+    public void searchView_isDisplayed(){
+        onView(withId(R.id.menu_search)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void fabOpen_isDisplayed(){
+        onView(withId(R.id.floating_open)).check(matches(isDisplayed()));
     }
 }
